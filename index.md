6ac02cbcc08c99f369d283aa3c508172cbc032eb
Mech24689 <95493916+Mech24689@users.noreply.github.com>
2022-08-23 11:50:51 -0300

    Add files via upload

---
+++ index.md
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Mario Bros</title>
+</head>
+<body>
+    <div class="tela">
+        
+            <img src="img/mario-gif.gif" class="box">
+            <img src="img/cano.png" class="movel andar">
+            <img src="img/nuvem.png" class="nuvem move-nuvem">
+        
+        <div class="piso"></div>
+    </div>    
+    <script src="js/code.js"></script>
+</body>
+</html>
---
+++ js/code.js
@@ -0,0 +1,42 @@
+const box = document.querySelector('.box');
+const movel = document.querySelector('.movel')
+
+document.addEventListener('touchstart', function(e){
+        box.classList.add('jump')
+        
+        setTimeout(() => {
+            box.classList.remove('jump')
+        },1000)
+    
+    if(e.key === 'r'){
+        location.reload()
+    }
+}, false)
+
+document.addEventListener('keydown', function(e){
+    if(e.key === 'Enter' || e.key === 'ArrowUp'){
+        box.classList.add('jump')
+        
+        setTimeout(() => {
+            box.classList.remove('jump')
+        },1000)
+    }
+    
+    if(e.key === 'r'){
+        location.reload()
+    }
+}, false)
+
+
+const loop = setInterval(() => {
+    const movelPosition = movel.offsetLeft
+    const boxPositionBottom = +window.getComputedStyle(box).bottom.replace('px', '')
+    const boxPositionTop = +window.getComputedStyle(box).top.replace('px', '')
+    
+    if(movelPosition <= 90 && boxPositionBottom < 100 && movelPosition > 0){
+        movel.style.animation = 'none'
+        box.style.animation = 'none'
+        movel.style.left = `${movelPosition}px`
+        box.style.top = `${boxPositionTop}px`
+    }
+},10)
